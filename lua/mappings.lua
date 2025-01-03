e1e314f13c82af41f2139b534179b29eb0cd7e49
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-03 13:35:22 +0100

    integrates visidata and relevant commands

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,14 @@ local map = vim.keymap.set
 -- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local utils = require("utils.functions")
+
+vim.api.nvim_set_keymap("n", "<leader>vd", "<cmd>lua require('utils.functions').open_in_visidata()<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<Leader>db", ":DBUIToggle<CR>", { noremap = true, silent = true })
+
+
+vim.api.nvim_set_keymap("n", "<Leader>df", ":DBUIFindBuffer<CR>", { noremap = true, silent = true })
+
+
