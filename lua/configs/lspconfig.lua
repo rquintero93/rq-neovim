21345d5975b9248e0475c3c68b27b2ff6e36fda0
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-04 21:50:29 +0100

    pyright and formatting

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls","pyre","sqlls"}
+local servers = { "html", "cssls", "pyright", "sqlls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,230 +1,292 @@
 return {
-  {
+  { -- Autoformat
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    event = "VeryLazy",
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>fd",
+        function()
+          require("conform").format { async = true, lsp_format = "fallback" }
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = "never"
+        else
+          lsp_format_opt = "fallback"
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform can also run multiple formatters sequentially
+        python = { "isort", "black" },
+        sql = { "sqlfmt" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-{
-   "williamboman/mason.nvim",
-   opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "stylua",
-      "html-lsp",
-      "css-lsp",
-      "prettier",
-      "pyre",
-      "sqlls",
-      "sql-formatter",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pyright",
+        "sqlls",
+        "sqlfmt",
+        "black",
+        "isort",
+      },
     },
-   },
   },
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "python","sql"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "sql",
+        "diff",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "bash",
+      },
+    },
   },
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  {'karb94/neoscroll.nvim', lazy=false},
-{
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  { "karb94/neoscroll.nvim", lazy = false },
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-},
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    "echasnovski/mini.nvim",
     config = function()
       -- Better Around/Inside textobjects
       -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-    end
+      require("mini.ai").setup { n_lines = 500 }
+    end,
   },
 
-{
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'bqsql'}, lazy = true }, -- Optional
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-    -- vim.g.db_ui_default_query = 'SELECT * FROM "{table}"'
-    vim.g.db_ui_save_location = '~/Documents/Global Alumni/queries/'
-  end,
-},
-{
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-        'kristijanhusak/vim-dadbod-completion',
-      },
-      -- override the options table that is used in the `require("cmp").setup()` call
-      opts = function(_, opts)
-        -- opts parameter is the default options table
-        -- the function is lazy loaded so cmp is able to be required
-        local cmp = require "cmp"
-        -- modify the sources part of the options table
-        opts.sources = cmp.config.sources {
-          { name = "nvim_lsp", priority = 1000 },
-          { name = "luasnip", priority = 750 },
-          { name = "buffer", priority = 500 },
-          { name = "path", priority = 250 },
-          { name = "vim-dadbod-completion", priority = 700 }, -- add new source
-        }
-        -- return the new table to be used
-        return opts
-      end,
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "bqsql" }, lazy = true }, -- Optional
     },
-{'github/copilot.vim',
-event = "InsertEnter",
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      -- vim.g.db_ui_default_query = 'SELECT * FROM "{table}"'
+      vim.g.db_ui_save_location = "~/Documents/Global Alumni/queries/"
+    end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+    },
+    -- override the options table that is used in the `require("cmp").setup()` call
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      -- modify the sources part of the options table
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+        { name = "vim-dadbod-completion", priority = 700 }, -- add new source
+      }
+      -- return the new table to be used
+      return opts
+    end,
+  },
+  { "github/copilot.vim", event = "InsertEnter" },
 
-{
-  "hkupty/iron.nvim",
+  {
+    "hkupty/iron.nvim",
     event = "VeryLazy",
-  config = function()
-    local iron = require("iron.core")
+    config = function()
+      local iron = require "iron.core"
 
-    iron.setup({
-      config = {
-        -- Whether a repl should be discarded or not
-        scratch_repl = true,
-        -- Your repl definitions come here
-        repl_definition = {
-          sh = {
-            -- Can be a table or a function that
-            -- returns a table (see below)
-            command = { "zsh" },
+      iron.setup {
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "zsh" },
+            },
+            python = {
+              command = { "ipython", "--no-autoindent" },
+              format = require("iron.fts.common").bracketed_paste_python,
+            },
           },
-          python = {
-            command = { "ipython" ,"--no-autoindent" },
-            format = require("iron.fts.common").bracketed_paste_python
-          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          -- repl_open_cmd = require("iron.view").bottom(40),
+          repl_open_cmd = require("iron.view").split.vertical.botright "40%",
         },
-        -- How the repl window will be displayed
-        -- See below for more information
-        -- repl_open_cmd = require("iron.view").bottom(40),
-        repl_open_cmd = require("iron.view").split.vertical.botright("40%"),
-      },
-      -- Iron doesn't set keymaps by default anymore.
-      -- You can set them here or manually add keymaps to the functions in iron.core
-      keymaps = {
-        send_motion = "<space>sc",
-        visual_send = "<space>ss",
-        send_file = "<space>sf",
-        send_line = "<space>sl",
-        send_mark = "<space>sm",
-        mark_motion = "<space>mc",
-        mark_visual = "<space>mc",
-        remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
-      },
-      -- If the highlight is on, you can change how it looks
-      -- For the available options, check nvim_set_hl
-      highlight = {
-        italic = true,
-      },
-      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-    })
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          send_motion = "<space>sc",
+          visual_send = "<space>ss",
+          send_file = "<space>sf",
+          send_line = "<space>sl",
+          send_mark = "<space>sm",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>s<space>",
+          exit = "<space>sq",
+          clear = "<space>cl",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      }
 
-    -- iron also has a list of commands, see :h iron-commands for all available commands
-    vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-    vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
-  end,
-},
-{'tpope/vim-fugitive',
+      -- iron also has a list of commands, see :h iron-commands for all available commands
+      vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+      vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+      vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+      vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+    end,
+  },
+  { "tpope/vim-fugitive", event = "VeryLazy" },
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      {
+        "]h",
+        function()
+          require("notebook-navigator").move_cell "d"
+        end,
+      },
+      {
+        "[h",
+        function()
+          require("notebook-navigator").move_cell "u"
+        end,
+      },
+      { "<leader>J", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+      { "<leader>j", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      "hkupty/iron.nvim", -- repl provider
+      "anuvyklack/hydra.nvim",
+    },
     event = "VeryLazy",
+    config = function()
+      local nn = require "notebook-navigator"
+      nn.setup { activate_hydra_keys = "<leader>hy" }
+    end,
   },
-{
-  "GCBallesteros/NotebookNavigator.nvim",
-  keys = {
-    { "]h", function() require("notebook-navigator").move_cell "d" end },
-    { "[h", function() require("notebook-navigator").move_cell "u" end },
-    { "<leader>J", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-    { "<leader>j", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-  },
-  dependencies = {
-    "echasnovski/mini.comment",
-    "hkupty/iron.nvim", -- repl provider
-    "anuvyklack/hydra.nvim",
-  },
-  event = "VeryLazy",
-  config = function()
-    local nn = require "notebook-navigator"
-    nn.setup({ activate_hydra_keys = "<leader>hy" })
-  end,
-},
-{
-  "echasnovski/mini.hipatterns",
-  event = "VeryLazy",
-  dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
-  opts = function()
-    local nn = require "notebook-navigator"
+  {
+    "echasnovski/mini.hipatterns",
+    event = "VeryLazy",
+    dependencies = { "GCBallesteros/NotebookNavigator.nvim" },
+    opts = function()
+      local nn = require "notebook-navigator"
 
-    local opts = { highlighters = { cells = nn.minihipatterns_spec } }
-    return opts
-  end,
-},
-{
+      local opts = { highlighters = { cells = nn.minihipatterns_spec } }
+      return opts
+    end,
+  },
+  {
     "kdheepak/lazygit.nvim",
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
     -- optional for floating window border decoration
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     -- keys = {
     --     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     -- }
-},
-{
-    'MeanderingProgrammer/render-markdown.nvim',
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
     lazy = false,
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
-}
+  },
+
+  -- Highlight todo, notes, etc in comments
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+  },
 }
