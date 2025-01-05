e0a0c0b1de655027a4e57d83248d5104df4f3ce0
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-05 15:58:53 +0100

    Copilot chat enabled

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -188,6 +188,21 @@ return {
     end,
   },
   {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" },
+        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      },
+      build = "make tiktoken", -- Only on MacOS or Linux
+      opts = {
+        -- See Configuration section for options
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "kristijanhusak/vim-dadbod-completion",
