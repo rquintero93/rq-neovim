242103bd2fdac46632b9aae8b4e65ff1d6cd34d8
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-05 18:49:05 +0100

    edit gui

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -56,7 +56,7 @@ M.ui = {
     },
   },
 
-  telescope = { style = "borderless" }, -- borderless / bordered
+  telescope = { style = "bordered" }, -- borderless / bordered
 
   statusline = {
     enabled = true,
